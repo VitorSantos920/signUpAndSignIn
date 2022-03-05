7eb49dbc97854794dc966ececa8815d450f5f90f
Vitor Santos <pireshugo737@gmail.com>
2022-03-05 19:03:39 -0300

    Versão atual do projeto: signup screen finalizada, iniciando signin screen

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// New
+function showPassword() {
+    document.getElementById("password").type = "text";
+}
+function hiddenPassword() {
+    document.getElementById("password").type = "password";
+}
+
+function hiddenPasswordMove() {
+    document.getElementById("password").type = "password";
+}
+
+function hiddenPasswordMoveRepeat() {
+    document.getElementById("passwordRepeat").type = "password";
+}
+
+function showPasswordRepeat() {
+    document.getElementById("passwordRepeat").type = "text";
+}
+
+function hiddenPasswordRepeat() {
+    document.getElementById("passwordRepeat").type = "password";
+}
